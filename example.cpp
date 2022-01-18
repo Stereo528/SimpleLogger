46af04c845d2dfa45c5743e2f5f4567e34471236
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2022-01-17 19:27:34 -0700

    add log() function

--- example.cpp
+++ example.cpp
@@ -10,5 +10,11 @@ int main(){
     log.error("Something went wrong!");
     log.fatal("This is a fatal error!");
 
+    log.log(log.INFO, "This is an info message using log()!");
+    log.log(log.DEBUG, "This is a debug message using log()!");
+    log.log(log.WARNING, "This is a warning using log()!");
+    log.log(log.ERROR, "Something went wrong using log()!");
+    log.log(log.FATAL, "This is a fatal error using log()!");
+
     return 0;
 }
--- logger.h
+++ logger.h
@@ -4,6 +4,23 @@
 namespace SimpleLogger {
     class Logger {
         public: 
+
+            enum LogLevel {
+                DEBUG,
+                INFO,
+                WARNING,
+                ERROR,
+                FATAL
+            };
+
+            void log(LogLevel level, std::string message) {
+                if (level == INFO) { std::cout << "\x1B[32mINFO: " << message << "\x1B[0m\t\t" << std::endl; } 
+                else if (level == DEBUG) { std::cout << "DEBUG: " << message << std::endl; } 
+                else if (level == WARNING) { std::cout << "\x1B[33mWARN: " << message << "\x1B[0m\t\t" << std::endl; } 
+                else if (level == ERROR) { std::cout << "\x1B[31mERROR: " << message << "\x1B[0m\t\t"  << std::endl; } 
+                else if (level == FATAL) { std::cout << "\x1B[41m\x1B[37mFATAL: " << message << "\x1B[0m\t\t" << std::endl; }
+            }
+
             //The colors/ANSI codes for the logger break in Clion but seem to work everywhere else.
             void info(std::string message) {
                 std::cout << "\x1B[32mINFO: " << message << "\x1B[0m\t\t" << std::endl;
